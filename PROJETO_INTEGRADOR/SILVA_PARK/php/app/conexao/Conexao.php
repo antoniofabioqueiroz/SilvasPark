fca4a257dc147b03508bfe0a11675360ca53f564
Fabio Queiroz <138711710+antoniofabioqueiroz@users.noreply.github.com>
2024-06-24 22:13:18 -0300

    Update Conexao.php

--- PROJETO_INTEGRADOR/SILVA_PARK/php/app/conexao/Conexao.php
+++ PROJETO_INTEGRADOR/SILVA_PARK/php/app/conexao/Conexao.php
@@ -9,7 +9,7 @@ class Conexao {
 
    public static function getConexao() {
        if (!isset(self::$instance)) {
-           self::$instance = new PDO('mysql:host=localhost;dbname=DB_NAME', 'USER_NAME', 'PASSWD', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+           self::$instance = new PDO('mysql:host=localhost;dbname=github', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
            self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
            self::$instance->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
        }
