2ee8153814ea12f9d0717041a96d8a8f86a0d4f4
Fabio Queiroz <138711710+antoniofabioqueiroz@users.noreply.github.com>
2024-06-24 21:47:39 -0300

    Add files via upload

---
+++ PROJETO_INTEGRADOR/SILVA_PARK/php/registros/entsaiveiculos.php
@@ -0,0 +1,152 @@
+<?php
+include_once "../app/conexao/Conexao.php";
+include_once "../app/dao/UsuarioDAO.php";
+include_once "../app/model/Usuario.php";
+
+//instancia as classes
+$usuario = new Usuario();
+$usuariodao = new UsuarioDAO();
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>CRUD Simples PHP</title>
+    <link rel="stylesheet" href="../../assets/css/estilo.css">
+</head>
+
+<body>
+<br>
+<br>
+<br>    
+ <div class="container_">
+    <div class="cabeca_container">   
+                <p> Cadastro de Consórcio Empresa </p>
+    </div>
+        <form action="../app/controller/UsuarioController.php" method="POST">
+            <div class="row">
+                <div class="col-md-3">
+                <label>EMPRESA</label>
+                    <input type="text" name="nome" value="" autofocus class="form-control" require />
+                </div>
+                <div class="col-md-3">
+                    <label>CNPJ</label>
+                    <input type="text" name="sobrenome" value="" class="form-control" require />
+                </div>
+                <div class="col-md-2">
+                    <label>TAXA</label>
+                    <input type="number" name="idade" value="" class="form-control" require />
+                </div>
+                <div class="col-md-3">
+                    <label>MODALIDADE</label>
+                    <select name="sexo" class="form-control">
+                        <option value="M">MENSALISTA</option>
+                        <option value="C">CONSÓRCIO</option>
+                    </select>
+                </div>
+                <div class="col-md-2">
+                    <br>
+                    <button class="btn btn-primary" type="submit" name="cadastrar">Cadastrar</button>
+                </div>
+            </div>
+        </form>
+        <hr>
+        <div class="table-responsive">
+            <table class="table table-sm table-bordered table-hover">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>EMPRESA</th>
+                        <th>CNPJ</th>
+                        <th>TAXA</th>
+                        <th>MODALIDADE</th>
+                        <th>AÇÕES</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($usuariodao->read() as $usuario) : ?>
+                        <tr>
+                            <td><?= $usuario->getId() ?></td>
+                            <td><?= $usuario->getNome() ?></td>
+                            <td><?= $usuario->getSobrenome() ?></td>
+                            <td><?= $usuario->getIdade() ?></td>
+                            <td><?= $usuario->getSexo()?></td>
+                            <td class="text-center">
+                                <button class="btn  btn-warning btn-sm" data-toggle="modal" data-target="#editar><?= $usuario->getId() ?>">
+                                    Editar
+                                </button>
+                                <a href="../app/controller/UsuarioController.php?del=<?= $usuario->getId() ?>">
+                                <button class="btn  btn-danger btn-sm" type="button">Excluir</button>
+                                </a>
+                            </td>
+                        </tr>
+                        <!-- Modal -->
+                        <div class="modal fade" id="editar><?= $usuario->getId() ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-lg" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Editar</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <form action="../app/controller/UsuarioController.php" method="POST">
+                                            <div class="row">
+                                                <div class="col-md-5">
+                                                    <label>Nome</label>
+                                                    <input type="text" name="nome" value="<?= $usuario->getNome() ?>" class="form-control" require />
+                                                </div>
+                                                <div class="col-md-7">
+                                                    <label>Sobrenome</label>
+                                                    <input type="text" name="sobrenome" value="<?= $usuario->getSobrenome() ?>" class="form-control" require />
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-md-3">
+                                                    <label>Idade</label>
+                                                    <input type="number" name="idade" value="<?= $usuario->getIdade() ?>" class="form-control" require />
+                                                </div>
+                                                <div class="col-md-3">
+                                                    <label>Sexo</label>
+                                                    <select name="sexo" class="form-control">
+                                                        <?php if ($usuario->getSexo() == 'F') : ?>
+                                                            <option value="F">Feminino</option>
+                                                            <option value="M">Masculino</option>
+                                                        <?php else : ?>
+                                                            <option value="M">Masculino</option>
+                                                            <option value="F">Feminino</option>
+                                                        <?php endif ?>
+
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-md-2">
+                                                    <br>
+                                                    <input type="hidden" name="id" value="<?= $usuario->getId() ?>" />
+                                                    <button class="btn btn-primary" type="submit" name="editar">Cadastrar</button>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    <?php endforeach ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
